922df3706f56131c777188e9ace717b9eb2c1044
jaiko <jaiko@toddwilson.net>
2013-02-04 23:31:12 -0500

    give aliases the same workflow as triggers now

--- sage/outbound.py
+++ sage/outbound.py
@@ -4,11 +4,16 @@ import sage
 
 def receiver(line):
 
+    sage.aliases.in_loop = True
+
     # run trigger matching over lines
     for alias in sage.aliases.enabled:
 
         # if we match, don't return the line. Expect the method to send for us.
         if alias.match(line):
             return None
+
+    sage.alaises.flush_set()
+    sage.aliases.in_loop = False
 
     return line
