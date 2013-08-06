b890b732bbd08911343af019da4f6b975922701f
jaiko <jaiko@toddwilson.net>
2013-08-06 14:41:43 -0400

    fix for a nasty bug with aliases and reloading

--- sage/outbound.py
+++ sage/outbound.py
@@ -6,14 +6,17 @@ def receiver(line):
 
     sage.aliases.in_loop = True
 
+    out = line
+
     # run trigger matching over lines
     for alias in sage.aliases.enabled:
 
         # if we match, don't return the line. Expect the method to send for us.
         if alias.match(line):
-            return None
+            out = None
+            break
 
     sage.aliases.flush_set()
     sage.aliases.in_loop = False
 
-    return line
+    return out
