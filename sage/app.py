5634ed3677c8f529bf1d8735b250efb07382faa1
Anonymous <anonymous@users.noreply.github.com>
2019-12-08 04:04:37 -0600

    App reload changes

--- sage/app.py
+++ sage/app.py
@@ -19,19 +19,30 @@ class AppNotLoaded(Exception):
     pass
 
 class App(object):
-    def __init__(self, name, namespace, path, manager):
+    def __init__(self, name, spec, manager):
         self.name = name
         self.fullname = None
-        self.namespace = namespace
-        self.path = path
+
         self.module = None
         self.matchables = []
         self.subapps = None
+
+        self.spec = spec
+        self.path = spec.submodule_search_locations[0]
         self.manager = manager
+        self.manager.paths[self.path] = name
 
-        self.manager.paths[path] = name
+    def load(self):
+        try:
+            self.namespace = importlib.util.module_from_spec(self.spec)
+        except ImportError:
+            try:
+                self.namespace = imports.import_file(name)
+            except ImportError:
+                sage._log.err("Error: Unable to import app '%s'" % name)
+                return
 
-        meta = importlib.import_module(f'{self.namespace}.meta')
+        meta = importlib.import_module(f'{self.name}.meta')
         if hasattr(meta, 'installed_apps'):
             if os.path.exists(self.path + '/apps') or os.path.isdir(self.path + '/apps'):
                 sys.path.append(self.path + '/apps')
@@ -47,16 +58,25 @@ class App(object):
             else:
                 self.fullname = '%s %s' % (self.fullname, meta.version)
 
-    def load(self):
-        self.module = importlib.import_module(f'{self.namespace}.{self.name}')
+        self.load_subapps()
+        self.module = importlib.import_module(f'{self.name}.{self.name}')
         if hasattr(self.module, 'init'):
-            self.module.init()
+            try:
+                self.module.init()
+            except:
+                sage._log.msg("Error in '%s' init" % self.name)
+                sage._log.err()
+                return False
 
         return True
 
     def post_init(self):
         if hasattr(self.module, 'post_init'):
-            self.module.post_init()
+            try:
+                self.module.post_init()
+            except:
+                sage._log.msg("Error in '%s' post-init" % self.name)
+                sage._log.err()
 
     def load_subapps(self):
         if self.subapps is not None:
@@ -68,7 +88,11 @@ class App(object):
 
     def reload(self):
         if hasattr(self.module, 'pre_reload'):
-            self.module.pre_reload()
+            try:
+                self.module.pre_reload()
+            except:
+                sage._log.msg("Error in '%s' pre-reload" % self.name)
+                sage._log.err()
 
         for group in self.matchables:
             group.destroy()
@@ -94,11 +118,19 @@ class App(object):
 
     def post_reload(self):
         if hasattr(self.module, 'post_reload'):
-            self.module.post_reload()
+            try:
+                self.module.post_reload()
+            except:
+                sage._log.msg("Error in '%s' post-reload" % self.name)
+                sage._log.err()
 
     def unload(self):
         if hasattr(self.module, 'unload'):
-            self.module.unload()
+            try:
+                self.module.unload()
+            except:
+                sage._log.msg("Error in '%s' unload" % self.name)
+                sage._log.err()
 
     def add_matchables(self, group):
         self.matchables.append(group)
@@ -151,33 +183,24 @@ class AppManager(dict):
 
         self[app].remove_matchables(group)
 
-    def load(self, name):
-        """ Load the root sage app """
-        with imports.cwd_in_path():
-            try:
-                ns = importlib.import_module(name)
-            except ImportError:
-                try:
-                    ns = imports.import_file(name)
-                except ImportError:
-                    sage._log.err("Error: Unable to import app '%s'" % name)
-                    return
-
-        self.load_app(name, ns)
+    def load_apps(self, root_app):
+        self.load(root_app)
         for app in self:
             self[app].post_init()
 
-    def load_app(self, name, namespace):
+    def load(self, name):
         """ Load a sage app """
-        path = os.path.dirname(inspect.getabsfile(namespace))
-        self[name] = App(name, namespace.__name__, path, self)
-        self[name].load_subapps()
+        spec = importlib.util.find_spec(name)
+        if not spec:
+            sage._log.err("Error: Unable to locate app '%s'" % name)
+        self[name] = App(name, spec, self)
+
         if self[name].load():
             sage._log.msg("Loaded app '%s'" % self[name].fullname)
-            return True
+            return
 
         sage._log.msg("Failed to load app '%s'" % name)
-        return False
+        return
 
     def load_manifest(self):
         for app in sage.manifest.apps:
